94fdd210fc2f2fb1f3fe6b0a4dfc5fce121b22e4
Jan Krems <jan.krems@groupon.com>
2015-10-18 16:43:59 -0700

    Port browser unit tests

---
+++ test/browser/cookie.test.js
@@ -0,0 +1,70 @@
+import assert from 'assertive';
+import { extend, noop } from 'lodash';
+
+import CookieMixin from '../../lib/browser/cookie';
+
+describe('cookie', () => {
+  describe('#setCookie', () => {
+    var cookie = extend({
+      driver: {
+        setCookie() {}
+      }
+    }, CookieMixin);
+
+    it('fails if cookie is undefined', () => {
+      assert.throws(() => cookie.setCookie(undefined));
+    });
+
+    it('fails if cookie does not contain name', () => {
+      assert.throws(() => cookie.setCookie({ value: 'chicago' }));
+    });
+
+    it('fails if cookie does not contain value', () => {
+      assert.throws(() => cookie.setCookie({ name: 'division' }));
+    });
+  });
+
+  describe('#getCookie', () => {
+    var cookie = extend({
+      driver: {
+        getCookies() { return []; }
+      }
+    }, CookieMixin);
+
+    it('fails if name is undefined', () => {
+      assert.throws(() => cookie.getCookie(undefined));
+    });
+
+    it('fails if name is not a String', () => {
+      assert.throws(() => cookie.getCookie(noop));
+    });
+
+    it('succeeds if name is a String', () => {
+      cookie.getCookie('division');
+    });
+  });
+
+  describe('#getHeader', () => {
+    var testiumCookie = {
+      name: '_testium_',
+      value: 'eyJoZWFkZXJzIjp7InNlcnZlciI6Im5vZGUtc3RhdGljLzAuNy4wIiwiY2FjaGUtY29udHJvbCI6Im1heC1hZ2U9MzYwMCIsImV0YWciOiJcIjE1ODI5ODQtMjUyNi0xMzkxNTI5MDM2MDAwXCIiLCJkYXRlIjoiTW9uLCAwMyBNYXIgMjAxNCAwNDo0MDoxNCBHTVQiLCJsYXN0LW1vZGlmaWVkIjoiVHVlLCAwNCBGZWIgMjAxNCAxNTo1MDozNiBHTVQiLCJjb250ZW50LXR5cGUiOiJ0ZXh0L2h0bWwiLCJjb250ZW50LWxlbmd0aCI6IjI1MjYiLCJjb25uZWN0aW9uIjoia2VlcC1hbGl2ZSIsIkNhY2hlLUNvbnRyb2wiOiJuby1zdG9yZSJ9LCJzdGF0dXNDb2RlIjoyMDB9',
+    };
+    var cookie = extend({
+      driver: {
+        getCookies() { return [ testiumCookie ]; }
+      }
+    }, CookieMixin);
+
+    it('fails if name is undefined', () => {
+      assert.throws(() => cookie.getHeader(undefined));
+    });
+
+    it('fails if name is not a String', () => {
+      assert.throws(() => cookie.getHeader(noop));
+    });
+
+    it('succeeds if name is a String', () => {
+      cookie.getHeader('user-agent');
+    });
+  });
+});
---
+++ test/browser/element.test.js
@@ -0,0 +1,92 @@
+import assert from 'assertive';
+import { extend, noop } from 'lodash';
+
+import ElementMixin from '../../lib/browser/element';
+
+describe('element', () => {
+  describe('#getElement', () => {
+    var element = extend({
+      driver: {
+        getElement() {}
+      }
+    }, ElementMixin);
+
+    it('fails if selector is undefined', () => {
+      assert.throws(() => element.getElement(undefined));
+    });
+
+    it('fails if selector is not a String', () => {
+      assert.throws(() => element.getElement(noop));
+    });
+
+    it('succeeds if selector is a String', () => {
+      element.getElement('.box');
+    });
+  });
+
+  describe('#waitForElementVisible', () => {
+    var element = extend({
+      driver: {
+        setElementTimeout() {},
+        getElement() {
+          return { isVisible() { return true; } };
+        }
+      }
+    }, ElementMixin);
+
+    it('fails if selector is undefined', () => {
+      assert.throws(() => element.waitForElementVisible(undefined));
+    });
+
+    it('fails if selector is not a String', () => {
+      assert.throws(() => element.waitForElementVisible(noop));
+    });
+
+    it('succeeds if selector is a String', () => {
+      element.waitForElementVisible('.box');
+    });
+  });
+
+  describe('#waitForElementNotVisible', () => {
+    var element = extend({
+      driver: {
+        setElementTimeout() {},
+        getElement() { return { isVisible() { return false; } } }
+      }
+    }, ElementMixin);
+
+    it('fails if selector is undefined', () => {
+      assert.throws(() => element.waitForElementNotVisible(undefined));
+    });
+
+    it('fails if selector is not a String', () => {
+      assert.throws(() => element.waitForElementNotVisible(noop));
+    });
+
+    it('succeeds if selector is a String', () => {
+      element.waitForElementNotVisible('.box');
+    });
+  });
+
+  describe('#click', () => {
+    var element = extend({
+      driver: {
+        getElement() {
+          return { click() {} };
+        }
+      }
+    }, ElementMixin);
+
+    it('fails if selector is undefined', () => {
+      assert.throws(() => element.click(undefined));
+    });
+
+    it('fails if selector is not a String', () => {
+      assert.throws(() => element.click(noop));
+    });
+
+    it('succeeds if selector is a String', () => {
+      element.click('.box');
+    });
+  });
+});
